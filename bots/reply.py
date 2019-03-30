d3df26dd104a29ef42387aadddb304ca931f9f63
Palash A <palash.agarwal.96@gmail.com>
2019-03-31 04:55:29 +0530

    rich replies

--- bots/reply.py
+++ bots/reply.py
@@ -55,12 +55,13 @@ async def help(ctx, *args):
 
 @bot.command(pass_context=True)
 async def reply(ctx, *args):
-  reply_to_id = ctx.message.content.split()[0][1:]
+  reply_to_id = ctx.message.content.split()[0][1:]; em = None
   try: 
     reply_to = await ctx.message.channel.fetch_message(reply_to_id)
     em = create_embed(reply_to, ctx.message)
   except discord.NotFound: 
     await ctx.message.channel.send('Message ID: {0} not found in channel'.format(reply_to_id))
+    await ctx.invoke(help)
   except discord.Forbidden: 
     await ctx.message.channel.send('I do not have access to the message being referred to!')
   await ctx.message.channel.send(embed=em)
@@ -69,14 +70,28 @@ async def reply(ctx, *args):
 ''' Helpers '''
 
 def create_embed(reply_to, reply):
-  # TODO Rich embeds for images and videos
-    # Use Embed.description for the file?
+
   reply_msg = ' '.join(reply.clean_content.split(' ')[1:])
+  embeds = reply_to.embeds
+  attached = reply_to.attachments
+
   em = discord.Embed()
   em.title = ':point_up_2: {0.author.display_name}\'s message is a reply to {1.author.display_name} :point_down:'.format(reply,reply_to)
   em.color = 0x44BFC1
   em.timestamp = reply.created_at
-  em.add_field(name='Original Message',value='[{0.clean_content}]({0.jump_url})'.format(reply_to),inline=False)
+
+  if attached and reply_msg:
+    em.add_field(name='Original Message',value='[Embedded content]({0.jump_url})'.format(reply_to),inline=False)
+    for ext in ['.jpg','.jpeg','.png','.gif']:
+      if attached[0].url.endswith(ext): em.set_image(url=attached[0].url); break
+    else:
+      for f in attached: em.add_field(name='Attached file', value='{0.url}'.format(f))
+  elif embeds and reply_msg:
+    if reply_to.clean_content: em.add_field(name='Original Message',value='[{0.clean_content}]({0.jump_url})'.format(reply_to),inline=False)
+    else: em.add_field(name='Original Message',value='[Embedded content]({0.jump_url})'.format(reply_to),inline=False)
+  elif reply_msg: em.add_field(name='Original Message',value='[{0.clean_content}]({0.jump_url})'.format(reply_to),inline=False)
+  else: pass
+
   em.add_field(name='Reply',value='[{0}]({1.jump_url})'.format(reply_msg,reply),inline=False)
   em.set_footer(text='Click the links to jump to the messages')
   return em
