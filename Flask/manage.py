0a03a0d4bf6419a505ed6c6a7a969491b7edd302
shreyaspapi <spapinwar@gmail.com>
2019-03-30 18:10:12 +0530

    more of structuring

--- Flask/manage.py
+++ Flask/manage.py
@@ -1,16 +1,20 @@
 from flask import Flask, render_template
 from flask import json
 import globals
+from home inport home
 
 app = Flask(__name__)
 
+app.register_blueprint(home)
+
 @app.route("/")
 def main():
-    return render_template('login.html', base_url = globals.base_url)
+    return render_template('index.html', base_url = globals.base_url)
 
 @app.route("/home")
 def home():
-	return render_template('home.html', base_url = globals.base_url)
+	return render_template('index.html', base_url = globals.base_url)
 
-
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int("80"),debug = True)
 
