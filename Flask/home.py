c106fa270bb3b029e17522998983bb53a04e9817
shreyaspapi <spapinwar@gmail.com>
2019-03-30 22:59:06 +0530

    Adding OAuth

--- Flask/home.py
+++ Flask/home.py
@@ -1,16 +1,88 @@
-from flask import Flask, render_template
-from flask import json
-from flask import Blueprint
+from flask import Flask, g, session, redirect, request, url_for, jsonify, render_template
+from requests_oauthlib import OAuth2Session
 import globals
-
-home = Blueprint("home", __name__)
-
-@home.route("/home/students")
-def create_students():
-    return
-
-@home.route("/home/courses")
-def create_courses():
-    return
+import os
+from home import home
 
 
+app = Flask(__name__)
+
+app.register_blueprint(home)
+
+@app.route("/")
+def main():
+    return render_template('index.html', base_url = globals.base_url)
+
+@app.route("/index")
+def base():
+	return render_template('index.html', base_url = globals.base_url)
+
+
+OAUTH2_CLIENT_ID = globals.OAUTH2_CLIENT_ID
+OAUTH2_CLIENT_SECRET = globals.OAUTH2_CLIENT_SECRET
+OAUTH2_REDIRECT_URI = 'http://10.12.4.147/index'
+
+API_BASE_URL = os.environ.get('API_BASE_URL', 'https://discordapp.com/api')
+AUTHORIZATION_BASE_URL = API_BASE_URL + '/oauth2/authorize'
+TOKEN_URL = API_BASE_URL + '/oauth2/token'
+
+app.debug = True
+app.config['SECRET_KEY'] = OAUTH2_CLIENT_SECRET
+
+if 'http://' in OAUTH2_REDIRECT_URI:
+    globals.OAUTHLIB_INSECURE_TRANSPORT = 'true'
+
+
+def token_updater(token):
+    session['oauth2_token'] = token
+
+
+def make_session(token=None, state=None, scope=None):
+    return OAuth2Session(
+        client_id=OAUTH2_CLIENT_ID,
+        token=token,
+        state=state,
+        scope=scope,
+        redirect_uri=OAUTH2_REDIRECT_URI,
+        auto_refresh_kwargs={
+            'client_id': OAUTH2_CLIENT_ID,
+            'client_secret': OAUTH2_CLIENT_SECRET,
+        },
+        auto_refresh_url=TOKEN_URL,
+        token_updater=token_updater)
+
+
+@app.route('/auth')
+def index():
+    scope = request.args.get(
+        'scope',
+        'identify email connections guilds guilds.join')
+    discord = make_session(scope=scope.split(' '))
+    authorization_url, state = discord.authorization_url(AUTHORIZATION_BASE_URL)
+    session['oauth2_state'] = state
+    return redirect(authorization_url)
+
+
+@app.route('/callback')
+def callback():
+    if request.values.get('error'):
+        return request.values['error']
+    discord = make_session(state=session.get('oauth2_state'))
+    token = discord.fetch_token(
+        TOKEN_URL,
+        client_secret=OAUTH2_CLIENT_SECRET,
+        authorization_response=request.url)
+    session['oauth2_token'] = token
+    return redirect(url_for('.me'))
+
+
+@app.route('/me')
+def me():
+    discord = make_session(token=session.get('oauth2_token'))
+    user = discord.get(API_BASE_URL + '/users/@me').json()
+    guilds = discord.get(API_BASE_URL + '/users/@me/guilds').json()
+    connections = discord.get(API_BASE_URL + '/users/@me/connections').json()
+    return jsonify(user=user, guilds=guilds, connections=connections)
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=int("80"),debug = True)
